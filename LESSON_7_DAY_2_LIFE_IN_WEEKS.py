df4f0e8b3ba43adc8ac6b17a2f6a9f511a378da9
raidsb <106170457+raidsb@users.noreply.github.com>
2024-01-08 19:56:16 +0800

    Create LESSON_7_DAY_2_LIFE_IN_WEEKS.py

---
+++ LESSON_7_DAY_2_LIFE_IN_WEEKS.py
@@ -0,0 +1,7 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+age_left = 90 - int(age)
+weeks_left = age_left * 52
+msg = f"You have {weeks_left} weeks left."
+print(msg)
