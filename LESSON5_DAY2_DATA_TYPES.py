f1b26eea9409ffffb9bddc351233af16a914028a
raidsb <106170457+raidsb@users.noreply.github.com>
2024-01-08 19:34:32 +0800

    Create LESSON5_DAY2_DATA_TYPES.py

---
+++ LESSON5_DAY2_DATA_TYPES.py
@@ -0,0 +1,8 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+first_digit = int(two_digit_number[0])
+second_digit= int(two_digit_number[1])
+
+print(first_digit + second_digit)
